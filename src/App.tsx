ddfdd2c7d986f841f297ce7b4853538ece9a9347
h1karoo <o.filipow@yandex.ru>
2022-12-04 23:13:21 +0700

    rtk query begin

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,24 @@
 import React, { useEffect } from 'react';
 import './App.css';
+import PostContainer from './components/PostContainer';
 import { useAppDispatch, useAppSelector } from './hooks/redux';
 import { fetchUsers } from './store/reducers/ActionCreators';
 import { userSlice } from './store/reducers/UserSlice';
 
 function App() {
-  const dispatch = useAppDispatch()
-  const {users, isLoading, error} = useAppSelector(state => state.userReducer)
+  // const dispatch = useAppDispatch()
+  // const {users, isLoading, error} = useAppSelector(state => state.userReducer)
 
-  useEffect( () => {
-    dispatch(fetchUsers())
-  }, [ ])
+  // useEffect( () => {
+  //   dispatch(fetchUsers())
+  // }, [ ])
 
   return (
     <div className="App">
-      {isLoading && <h1>Идет загрузка...</h1>}
+      {/* {isLoading && <h1>Идет загрузка...</h1>}
       {error && <h1>{error}</h1>}
-      {JSON.stringify(users, null, 2)}
+      {JSON.stringify(users, null, 2)} */}
+      <PostContainer/>
     </div>
   );
 }
---
+++ src/components/PostContainer.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import { postAPI } from '../services/PostService';
+import PostItem from './PostItem';
+
+const PostContainer = () => {
+    const {data: posts, error, isLoading} = postAPI.useFetchAllPostsQuery(10)
+    return (
+        <div>
+            <div className="post__list">
+                {isLoading && <h1>Идет загрузка...</h1>}
+                {error && <h1>Ошибка!!!</h1>}
+                {posts && posts.map(post =>
+                    <PostItem key = {post.id} post = {post} />
+                )}
+            </div>
+        </div>
+    );
+};
+
+export default PostContainer;
