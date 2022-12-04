b1d72c2401cc7d8f760340546a04a62313f0ab48
h1karoo <o.filipow@yandex.ru>
2022-12-05 00:28:51 +0700

    add deletePost and updatePost

--- src/components/PostContainer.tsx
+++ src/components/PostContainer.tsx
@@ -1,10 +1,14 @@
 import React, {useState, useEffect} from 'react';
+import { IPost } from '../models/IPost';
 import { postAPI } from '../services/PostService';
 import PostItem from './PostItem';
 
 const PostContainer = () => {
-    const [limit, setLimit] = useState(10);
+    const [limit, setLimit] = useState(100);
     const {data: posts, error, isLoading, refetch} = postAPI.useFetchAllPostsQuery(limit)
+    const [createPost, {}] = postAPI.useCreatePostMutation()
+    const [updatePost, {}] = postAPI.useUpdatePostMutation()
+    const [deletePost, {}] = postAPI.useDeletePostMutation()
 
     useEffect(() => {
         // setTimeout(() => {
@@ -12,13 +16,26 @@ const PostContainer = () => {
         // }, 2000)
     }) 
 
+    const handlerCreate = async () => {
+        const title = prompt()
+        await createPost({title, body: title} as IPost)
+    }
+
+    const handleRemove = (post: IPost) => {
+        deletePost(post)
+    }
+    const handleUpdate = (post: IPost) => {
+        updatePost(post)
+    }
+
     return (
         <div>
             <div className="post__list">
+                <button onClick={handlerCreate}>Add new post</button>
                 {isLoading && <h1>Идет загрузка...</h1>}
                 {error && <h1>Ошибка!!!</h1>}
                 {posts && posts.map(post =>
-                    <PostItem key = {post.id} post = {post} />
+                    <PostItem remove={handleRemove} update = {handleUpdate}key = {post.id} post = {post} />
                 )}
             </div>
         </div>
--- src/components/PostContainer2.tsx
+++ src/components/PostContainer2.tsx
@@ -3,15 +3,15 @@ import { postAPI } from '../services/PostService';
 import PostItem from './PostItem';
 
 const PostContainer2 = () => {
-    const {data: posts, error, isLoading} = postAPI.useFetchAllPostsQuery(10)
+    const {data: posts, error, isLoading} = postAPI.useFetchAllPostsQuery(100)
     return (
         <div>
             <div className="post__list">
                 {isLoading && <h1>Идет загрузка...</h1>}
                 {error && <h1>Ошибка!!!</h1>}
-                {posts && posts.map(post =>
+                {/* {posts && posts.map(post =>
                     <PostItem key = {post.id} post = {post} />
-                )}
+                )} */}
             </div>
         </div>
     );
